5bb0e3f68f9b942a056c2e36c2e2454274936686
nicolasblond@web-designers.fr <nicolasblond@web-designers.fr>
2022-05-30 17:21:04 +0200

    Project init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: ['@pinia/nuxt','@nuxtjs/vuetify','@nuxtjs/axios'],
-    modules: ['@nuxtjs/axios'],
+    buildModules: ['@pinia/nuxt'],
+    modules: [],
 })
